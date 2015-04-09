d3c903b8420511d16c3b880bf937f79ed79479da
rychuhardy <ryszard.sikora7@gmail.com>
2015-04-09 20:24:58 +0200

    Running engines

---
+++ src/main/java/RunCapo.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Ryœ on 2015-04-09.
+ */
+public class RunCapo {
+}
---
+++ src/main/java/RunCapoRun.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Ryœ on 2015-04-09.
+ */
+package pl.edu.agh.capo;
+
+import pl.edu.agh.amber.hokuyo.*;
+import pl.edu.agh.amber.common.AmberClient;
+import pl.edu.agh.amber.roboclaw.MotorsCurrentSpeed;
+import pl.edu.agh.amber.roboclaw.RoboclawProxy;
+
+public class RunCapoRun {
+
+    public static void main(String[] args) throws Exception {
+        //
+
+
+        AmberClient client = new AmberClient("192.168.2.203", 26233);
+        RoboclawProxy roboclawProxy = new RoboclawProxy(client, 0);
+
+        MotorsCurrentSpeed mcs;
+
+        for (int i = 1; i <= 10; i++) {
+            roboclawProxy.sendMotorsCommand(100 * i, 100 * i, 100 * i, 100 * i);
+
+            Thread.sleep(500);
+
+            mcs = roboclawProxy.getCurrentMotorsSpeed();
+            mcs.waitAvailable();
+            System.out.println(String.format("Motors current speed: fl: %d, fr: %d, rl: %d, rr: %d",
+                    mcs.getFrontLeftSpeed(), mcs.getFrontRightSpeed(), mcs.getRearLeftSpeed(), mcs.getRearRightSpeed()));
+
+        }
+
+
+
+        Thread.sleep(2000);
+
+        roboclawProxy.sendMotorsCommand(0, 0, 0, 0);
+        client.terminate();
+
+
+    }
+
+}
