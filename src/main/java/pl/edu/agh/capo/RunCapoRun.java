9a44eaa0d315eedf431447cb5422668c88e60f9b
rychuhardy <ryszard.sikora7@gmail.com>
2015-04-18 17:01:12 +0200

    Keyboard Steering -  not finished

--- src/main/java/pl/edu/agh/capo/RunCapoRun.java
+++ src/main/java/pl/edu/agh/capo/RunCapoRun.java
@@ -7,6 +7,7 @@ import pl.edu.agh.amber.hokuyo.*;
 import pl.edu.agh.amber.common.AmberClient;
 import pl.edu.agh.amber.roboclaw.MotorsCurrentSpeed;
 import pl.edu.agh.amber.roboclaw.RoboclawProxy;
+import pl.edu.agh.capo.Keyboard.KeyboardSteering;
 
 public class RunCapoRun {
 
@@ -16,26 +17,14 @@ public class RunCapoRun {
 
         AmberClient client = new AmberClient("192.168.2.203", 26233);
         RoboclawProxy roboclawProxy = new RoboclawProxy(client, 0);
+        SingleScan scann=new SingleScan(client, roboclawProxy, 0);
+        MotorsCurrentSpeed motorsCurrentSpeed=new MotorsCurrentSpeed(0,0,0,0);
 
-        MotorsCurrentSpeed mcs;
-
-        for (int i = 1; i <= 10; i++) {
-            roboclawProxy.sendMotorsCommand(100 * i, 100 * i, 100 * i, 100 * i);
-
-            Thread.sleep(500);
-
-            mcs = roboclawProxy.getCurrentMotorsSpeed();
-            mcs.waitAvailable();
-            System.out.println(String.format("Motors current speed: fl: %d, fr: %d, rl: %d, rr: %d",
-                    mcs.getFrontLeftSpeed(), mcs.getFrontRightSpeed(), mcs.getRearLeftSpeed(), mcs.getRearRightSpeed()));
-
-        }
-
-
-
-        Thread.sleep(2000);
-
-        roboclawProxy.sendMotorsCommand(0, 0, 0, 0);
+        //scann.printScan();
+        KeyboardSteering keyboardSteering=new KeyboardSteering(client, roboclawProxy, motorsCurrentSpeed, 0);
+        Thread thread=new Thread(keyboardSteering);
+        thread.start();
+        Thread.sleep(10000);
         client.terminate();
 
 
